1be5abda95fa27fc48dfff18fbbe4174926d4e11
gal <galchapman@gmail.com>
2021-03-08 00:21:56 +0200

    fix random error

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IndexTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IndexTest.java
@@ -6,7 +6,6 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.lib.MegiddoGamepad;
 import org.firstinspires.ftc.teamcode.subsystems.ShooterSubsystem;
 
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
