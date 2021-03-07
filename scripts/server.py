53150ac35d4fcca99baa1ba673c30cfb62da5c4f
gal <galchapman@gmail.com>
2021-03-07 15:54:40 +0200

    improve debug server code

--- scripts/server.py
+++ scripts/server.py
@@ -3,64 +3,56 @@
 import socket
 import struct
 import matplotlib.pyplot as plt
-import matplotlib
-import numpy as np
-import threading
 
-# matplotlib.use('Agg')
+
+PACKET_SIZE = 16
+DATA_SIZE = 100
+
 
 def show(time_list, data_list):
+    # clear plot
     plt.cla()
+    # plot data
     plt.plot(time_list, data_list)
-
+    # update plot
     plt.pause(0.00001)
 
-packet_size = 16
 
 if __name__ == '__main__':
 
-    # thread = threading.Thread(show, data_list, time_list)
-
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as listen_socket:
+        # open server on port 5038
         listen_socket.bind(('', 5038))
-
+        # listen for connections
         listen_socket.listen()
-
+        # for each connection
         while True:
-            client_socket, client_addr = listen_socket.accept()
-
-            data_list = [(0, 0)]
-            v_list = [(0,0, 0)]
-            time_list = [0]
-            
-            
+            # accept connection
+            client_socket, client_address = listen_socket.accept()
+            # reset time and data buffers
+            data_list = []
+            time_list = []
+            # for all packets from socket
             while True:
-                # wait for all the data
-                while len(client_socket.recv(packet_size, socket.MSG_PEEK)) < packet_size:
-                    if not client_socket.recv(packet_size, socket.MSG_PEEK):
+                try:
+                    # get data as binary
+                    data = client_socket.recv(PACKET_SIZE)
+                    # if data length is 0 there is error
+                    if not data:
                         break
-                
-                data = client_socket.recv(packet_size)
-
-                if not data:
+                    # convert binary data to time and entries
+                    time, v1, v2 = struct.unpack('dii'[::-1], data[::-1])
+                    # add time and data to buffers
+                    time_list.append(time)
+                    data_list.append((v1, v2))
+                    # plot data
+                    show(time_list, data_list)
+                    # remove old data
+                    if len(time_list) == DATA_SIZE:
+                        time_list.pop(0)
+                        data_list.pop(0)
+                # end of connection
+                except ConnectionResetError:
                     break
-                print(data)
-                time = struct.unpack('l', data[:8][::-1])[0] / 1000
-                left = struct.unpack('i', data[8:12][::-1])[0]
-                right = struct.unpack('i', data[12:16][::-1])[0]
-
-                v_list.append((left - data_list[-1][0], right - data_list[-1][1], (left - data_list[-1][0]+ right - data_list[-1][1])/2))
-
-                data_list.append((left, right))
-                time_list.append(time)
-
-                show(time_list, v_list)
-
-                if (len(data_list) >= 100): # about 10 secends
-                    data_list.pop(0)
-                    time_list.pop(0)
-                    v_list.pop(0)    
-
+            # close socket
             client_socket.close()
-            break
-            
