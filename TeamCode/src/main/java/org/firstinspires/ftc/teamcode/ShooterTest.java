749a6e6670c8e22cf24836756a6d1716a8afb07c
Megiddo <megiddo.lions@gmail.com>
2021-03-12 12:22:01 +0200

    add VelocityControl

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterTest.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
 
 import org.firstinspires.ftc.teamcode.lib.MegiddoGamepad;
 
